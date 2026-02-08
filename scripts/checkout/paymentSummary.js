ea6383f2532aae87f4c1e9cb702ba2a1a3b86a6d
plaisamudcha <plai_samudcha@hotmail.com>
2026-02-08 21:00:21 +0700

    implement itemQuantity

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,10 +6,12 @@ import formatCurrency from "../utils/money.js";
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let itemQuantity = 0;
 
   carts.forEach((element) => {
     const product = getProduct(element.productId);
     productPriceCents += product.priceCents * element.quantity;
+    itemQuantity += element.quantity;
 
     const deliveryOption = getDeliveryOption(element.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
@@ -23,7 +25,7 @@ export function renderPaymentSummary() {
       <div class="payment-summary-title">Order Summary</div>
 
       <div class="payment-summary-row">
-        <div>Items (3):</div>
+        <div>Items (${itemQuantity}):</div>
         <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
       </div>
 
